af107bae3674a28267103112695ae5130812ae93
ABHISHEK SINGH <singhaacs19@student.mes.ac.in>
2023-02-27 09:29:43 +0530

    new test update

---
+++ src/main/java/test2.java
@@ -0,0 +1,13 @@
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Tets 2");
+		System.out.println("Hello");
+		System.out.println("Hello");
+
+	}
+
+}
